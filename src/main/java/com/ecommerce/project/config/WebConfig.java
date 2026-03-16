66046df4405eda5ed01ee2a3146664a252b546a4
Shivam <shivamranjan68@gmail.com>
2026-03-16 23:16:30 +0530

    Minor Fixes in Web Config

--- src/main/java/com/ecommerce/project/config/WebConfig.java
+++ src/main/java/com/ecommerce/project/config/WebConfig.java
@@ -1,20 +1,29 @@
 package com.ecommerce.project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://192.168.209.159:5173/")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
 
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+//    }
 }
